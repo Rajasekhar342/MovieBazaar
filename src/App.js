8a10a9834851625be3325bdc46884222539e5b56
Rajasekhar <rajasekharbevara341@gmail.com>
2022-09-03 13:10:36 +0530

    changing the app interface

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ const App = () => {
     setMovies(data.Search)
   }
 
-  const movie1 = {
-    Title: 'Spiderman in Cannes',
-    Year: '2016',
-    imdbID: 'tt5978586',
-    Type: 'movie',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg',
-  }
+  // const movie1 = {
+  //   Title: 'Spiderman in Cannes',
+  //   Year: '2016',
+  //   imdbID: 'tt5978586',
+  //   Type: 'movie',
+  //   Poster:
+  //     'https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg',
+  // }
 
   useEffect(() => {
-    searchMovies('Spiderman')
+    searchMovies('Batman')
   }, [])
 
   return (
